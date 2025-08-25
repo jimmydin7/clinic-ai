6955464a270705c945059ec2509ac414cf4def15
jimmydin7 <turnspeaker@gmail.com>
2025-08-25 21:13:39 +0300

    finish cancer model!

--- models/cancer/cancer.py
+++ models/cancer/cancer.py
@@ -21,3 +21,14 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_
 model = RandomForestClassifier(random_state=42)
 model.fit(X_train, Y_train)
 
+def predict(patient_dataframe):
+    cancer_prediction = model.predict(patient_dataframe)
+
+    if cancer_prediction[0] == 1:
+        # this means cancer, now getting probability
+        probability = model.predict_proba(patient_dataframe)[0][1] * 100
+        return probability
+    
+    else:
+        # no cancer, patient clear!
+        return None
